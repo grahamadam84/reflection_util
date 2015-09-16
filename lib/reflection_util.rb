0951b7ebdd7196139758fd55a92bf5b743baf5ff
grahamadam84 <adam.graham@sage.com>
2015-09-16 07:11:58 +0100

    added rspec

--- lib/reflection_util.rb
+++ lib/reflection_util.rb
@@ -1,7 +1,7 @@
 class ReflectionUtil
 	def show_associations(model, key = nil)
 		
-		if model.class && model.class != Class && model.reflections
+		if model.class && model.class != Class && model.respond_to?(:reflections)
 			if key
 				model.class.reflections[key.to_sym]
 			else
