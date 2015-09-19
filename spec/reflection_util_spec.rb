78a87e5850e2992df3b97ad72d1d3ce8e5fb43bd
grahamadam84 <adam.graham@sage.com>
2015-09-19 16:47:06 +0100

    Add Rakefile

--- spec/reflection_util_spec.rb
+++ spec/reflection_util_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe ReflectionUtil do
 	it 'should return nil' do
-		require 'pry'; binding.pry
 		result = subject.show_associations(nil)
 		expect(result).to be_nil
 	end
